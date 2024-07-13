e6e6238472635ec06fbe39482de09fbb35c7ed6d
oriolpuerto <oriolpuerto@gmail.com>
2024-07-13 09:05:44 -0500

    Creando funcion convertir

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+
+    static void convertir (double valorMoneda, String divisa ) {
+        Scanner leer = new Scanner(System.in);
+        System.out.printf("Ingresa el valor a convertir: %s", divisa);
+        double cantidadMoneda = leer.nextDouble();
+
+        double
+    }
+}
